13e52bdc8e205fb2848f16d0b0473e349aee0a18
NikkiBeeLili <arcanahereticae13@gmail.com>
2021-02-03 16:59:30 -0600

    In the middle of 2:Controllers

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,6 +14,11 @@ import java.util.Optional;
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    //findAll(), save(), findById()(?)
 
     
 
@@ -25,8 +32,11 @@ public class EmployerController {
     @PostMapping("add")
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                     Errors errors, Model model) {
+        model.addAttribute("employers", employerRepository.findAll());
+
 
         if (errors.hasErrors()) {
+            employerRepository.save(newEmployer);
             return "employers/add";
         }
 
@@ -42,6 +52,7 @@ public class EmployerController {
             model.addAttribute("employer", employer);
             return "employers/view";
         } else {
+
             return "redirect:../";
         }
     }
