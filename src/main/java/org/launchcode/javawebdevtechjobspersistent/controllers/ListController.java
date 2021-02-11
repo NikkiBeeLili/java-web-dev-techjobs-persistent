6535dfc82d01d15bdee2807d166fb0508670d094
NikkiBeeLili <arcanahereticae13@gmail.com>
2021-02-10 20:41:37 -0600

    Needs the tables from Assignment 3.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
@@ -1,16 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.JobData;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
 import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-import org.launchcode.javawebdevtechjobspersistent.models.JobData;
-
+import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -21,6 +26,12 @@ public class ListController {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
 
     static HashMap<String, String> columnChoices = new HashMap<>();
 
@@ -51,5 +62,29 @@ public class ListController {
         model.addAttribute("jobs", jobs);
 
         return "list-jobs";
+    }
+    @GetMapping("add")
+    public String displayAddJobForm(Model model) {
+        model.addAttribute(new Job());
+        model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
+        return "add";
+    }
+
+    @PostMapping("add")
+    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
+                                    Errors errors, Model model, @RequestParam List<Integer> skills, @RequestParam int employerId) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Job");
+            return "add";
+        }
+        Employer employer = employerRepository.findById(employerId).orElse(new Employer());
+        newJob.setEmployer(employer);
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills((Skill) skillObjs);
+
+        return "redirect:";
     }
 }
